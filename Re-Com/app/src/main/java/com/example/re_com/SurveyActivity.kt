c3abcb24c8271fc0c7d76150361fd0104973d70c
M0710Fa <b1018188.fun@gmail.com>
2020-08-16 08:07:26 +0900

    [fix]画面遷修正

---
+++ Re-Com/app/src/main/java/com/example/re_com/SurveyActivity.kt
@@ -0,0 +1,40 @@
+package com.example.re_com
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_survey_button.*
+
+class SurveyActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_survey_button)
+
+        val serectWeight = arrayOf(-1,0,1,5,10)
+
+        var weightSum = 0
+
+        var clickTimes = 1
+
+
+        for(i in 0..1){
+
+            button_1.setOnClickListener {
+                Toast.makeText(this, "ふーん", Toast.LENGTH_SHORT).show()
+            }
+            button_2.setOnClickListener {
+                Toast.makeText(this, "本当にそれでいいの？", Toast.LENGTH_SHORT).show()
+
+            }
+            button_3.setOnClickListener {
+                Toast.makeText(this, "君ってスタバにmac持って行きそう", Toast.LENGTH_SHORT).show()
+
+            }
+            button_4.setOnClickListener {
+                Toast.makeText(this, "いいセンスしてるね！", Toast.LENGTH_SHORT).show()
+
+            }
+        }
+    }
+}
