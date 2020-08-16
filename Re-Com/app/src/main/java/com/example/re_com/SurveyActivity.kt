f766d3b1962258157d03d09b00023d27ec09e995
M0710Fa <b1018188.fun@gmail.com>
2020-08-16 19:39:11 +0900

    [add]結果の表示 [fix]レイアウトの編集

--- Re-Com/app/src/main/java/com/example/re_com/SurveyActivity.kt
+++ Re-Com/app/src/main/java/com/example/re_com/SurveyActivity.kt
@@ -1,40 +1,73 @@
 package com.example.re_com
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_survey_button.*
 
 class SurveyActivity : AppCompatActivity() {
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_survey_button)
+        val questionsArray: Array<String> = resources.getStringArray(R.array.questions)
+        val stringArray = arrayOf<String>()
+        var clickTimes = 0
+        var check = 0
+        button_1.text = questionsArray[0]
+        button_2.text = questionsArray[1]
+        button_3.text = questionsArray[2]
+        button_4.text = questionsArray[3]
+        button_5.text = questionsArray[4]
+        button_6.text = questionsArray[5]
+        button_7.text = questionsArray[6]
+        button_8.text = questionsArray[7]
 
-        val serectWeight = arrayOf(-1,0,1,5,10)
+        val intent = getIntent()
+        val intentSub = Intent()
 
-        var weightSum = 0
+        button_1.setOnClickListener {
+            intentSub.putExtra(MainActivity.EXTRA_MESSAGE, "high")
+            setResult(Activity.RESULT_OK, intentSub)
+            finish()
+        }
+        button_2.setOnClickListener {
+            intentSub.putExtra(MainActivity.EXTRA_MESSAGE, "middle")
+            setResult(Activity.RESULT_OK, intentSub)
+            finish()
+        }
+        button_3.setOnClickListener {
+            intentSub.putExtra(MainActivity.EXTRA_MESSAGE, "high")
+            setResult(Activity.RESULT_OK, intentSub)
+            finish()
+        }
+        button_4.setOnClickListener {
+            intentSub.putExtra(MainActivity.EXTRA_MESSAGE, "middle")
+            setResult(Activity.RESULT_OK, intentSub)
+            finish()
+        }
+        button_5.setOnClickListener {
+            intentSub.putExtra(MainActivity.EXTRA_MESSAGE, "mac")
+            setResult(Activity.RESULT_OK, intentSub)
+            finish()
+        }
+        button_6.setOnClickListener {
+            intentSub.putExtra(MainActivity.EXTRA_MESSAGE, "high")
+            setResult(Activity.RESULT_OK, intentSub)
+            finish()
+        }
+        button_7.setOnClickListener {
+            intentSub.putExtra(MainActivity.EXTRA_MESSAGE, "mac")
+            setResult(Activity.RESULT_OK, intentSub)
+            finish()
+        }
+        button_8.setOnClickListener {
+            intentSub.putExtra(MainActivity.EXTRA_MESSAGE, "low")
+            setResult(Activity.RESULT_OK, intentSub)
+            finish()
 
-        var clickTimes = 1
-
-
-        for(i in 0..1){
-
-            button_1.setOnClickListener {
-                Toast.makeText(this, "ふーん", Toast.LENGTH_SHORT).show()
-            }
-            button_2.setOnClickListener {
-                Toast.makeText(this, "本当にそれでいいの？", Toast.LENGTH_SHORT).show()
-
-            }
-            button_3.setOnClickListener {
-                Toast.makeText(this, "君ってスタバにmac持って行きそう", Toast.LENGTH_SHORT).show()
-
-            }
-            button_4.setOnClickListener {
-                Toast.makeText(this, "いいセンスしてるね！", Toast.LENGTH_SHORT).show()
-
-            }
         }
     }
 }
